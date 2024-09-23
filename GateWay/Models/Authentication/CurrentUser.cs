3037e8806155093f26a1703b8a9754d7ea00b045
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-23 17:18:36 +0100

    DTO added

---
+++ GateWay/Models/Authentication/CurrentUser.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Gateway.Client.Models.Authentication
+{
+    public class CurrentUser
+    {
+        public bool IsAuthenticated { get; set; }
+        public string UserName { get; set; }
+        public Dictionary<string, string> Claims { get; set; }
+    }
+}
---
+++ GateWay/Models/Authentication/ViewRegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gateway.Models.Authentication
+{
+    public  class ViewRegisterModel
+    {
+        [Required]
+        public string Email { get; set; } = default!;
+        [Required]
+        public string Password { get; set; } = default!;
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match!")]
+        public string RepeatPassword { get; set; } = default!;
+        [Required]
+        public string FirstName { get; set; } = default!;
+        [Required]
+        public string LastName { get; set; } = default!;
+    }
+}
