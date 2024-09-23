59948d31f690dd658c07c2f4e804e36d63eb10fc
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-23 17:18:20 +0100

    Authentication feature added

---
+++ GateWay/Services/Authentication/Authentication.cs
@@ -0,0 +1,24 @@
+﻿using Gateway.Client.Models.Authentication;
+using Gateway.Models.Authentication;
+using System.Net.Http.Json;
+
+
+namespace Gateway.Services.Authentication
+{
+    public class Authentication(HttpClient _httpClient) : IAuthentication
+    {
+        public async Task Register(ViewRegisterModel registerRequest)
+        {
+            var result = await _httpClient.PostAsJsonAsync("api/account/register", registerRequest);
+            if (result.StatusCode == System.Net.HttpStatusCode.BadRequest) throw new Exception(await result.Content.ReadAsStringAsync());
+            result.EnsureSuccessStatusCode();
+        }
+
+        public async Task<CurrentUser> CurrentUserInfo()
+        {
+            var result = await _httpClient.GetFromJsonAsync<CurrentUser>("api/authentication/currentuserinfo") ??
+            throw new ArgumentNullException("user not found");
+            return result;
+        }
+    }
+}
---
+++ GateWay/Services/Authentication/IAuthentication.cs
@@ -0,0 +1,12 @@
+﻿using Gateway.Client.Models.Authentication;
+using Gateway.Models.Authentication;
+
+
+namespace Gateway.Services.Authentication
+{
+    public interface IAuthentication
+    {
+        Task Register(ViewRegisterModel registerRequest);
+        Task<CurrentUser> CurrentUserInfo();
+    }
+}
