2ad1a84b0f06678c4693567c54a302aab0ab3fab
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-30 23:40:17 +0100

    Model Added

---
+++ GateWay/Models/QuestionGeneration/GenerateTextViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Gateway.ClientAI.Models.QuestionGeneration.QuestionViewModel;
+
+namespace Gateway.ClientAI.Models.QuestionGeneration
+{
+    public class GenerateTextViewModel
+    {
+        public class TextModel
+        {
+            [Required(ErrorMessage = "Title content is required.")]
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public Choice Choice { get; set; }
+        }
+
+        
+
+        public enum QuestionType
+        {
+            MultipleChoice = 1,
+            TrueOrFalse,
+        }
+
+       
+
+        public class Choice
+        {
+            public int NumberOfQuestion { get; set; }
+            public QuestionType Type { get; set; }
+        }
+
+        public class AssessmentService
+        {
+            public AssessmentResponse Assessment { get; set; }
+        }
+    }
+}
