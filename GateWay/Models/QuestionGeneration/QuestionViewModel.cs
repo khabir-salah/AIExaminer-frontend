1ffe1559c736f454a48f217052b7046b81bde62f
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-30 23:40:35 +0100

    Model added

---
+++ GateWay/Models/QuestionGeneration/QuestionViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using static Gateway.ClientAI.Models.QuestionGeneration.GenerateTextViewModel;
+
+namespace Gateway.ClientAI.Models.QuestionGeneration
+{
+    public class QuestionViewModel
+    {
+        public record class AssessmentResponse
+        {
+            public Guid AssessmentId { get; set; }
+            public ICollection<QuestionModel> Question { get; set; }
+            public QuestionType QuestionType { get; set; }
+            public DateTime Date { get; set; }
+        }
+        public record class QuestionModel
+        {
+            public Guid QuestionId { get; set; }
+            public string QuestionText { get; set; }
+            public ICollection<OptionModel> Options { get; set; }
+        }
+
+        public record class OptionModel
+        {
+            public Guid OptionId { get; set; }
+            public string Text { get; set; }
+        }
+    }
+}
