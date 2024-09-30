995c3c73427408ca56e89685d233dd39bfaefee6
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-30 23:41:02 +0100

    model added

---
+++ GateWay/Models/QuestionGeneration/ResultModel.cs
@@ -0,0 +1,12 @@
+﻿using static Gateway.ClientAI.Models.QuestionGeneration.SubmitAssessmentModel;
+
+namespace Gateway.ClientAI.Models.QuestionGeneration
+{
+    public class ResultModel
+    {
+        public class ResultViewModel
+        {
+            public AssessmentResultResponseModel AssessmentResult { get; set; }
+        }
+    }
+}
---
+++ GateWay/Models/QuestionGeneration/SubmitAssessmentModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Gateway.ClientAI.Models.QuestionGeneration.GenerateTextViewModel;
+
+namespace Gateway.ClientAI.Models.QuestionGeneration
+{
+    public class SubmitAssessmentModel
+    {
+        public class SubmitAssessmentRequest
+        {
+            public Dictionary<Guid, Guid> Answer { get; set; } 
+            public Guid AssesmentId { get; set; }
+        }
+
+        public record class QuestionResponseView
+        {
+            public string QuestionText { get; set; }
+            public ICollection<OptionResponseView> Options { get; set; }
+            public string Answer { get; set; }
+            public string Explanation { get; set; }
+            public string UserAnswer { get; set; }
+        }
+
+        public record class OptionResponseView
+        {
+            public string Text { get; set; }
+        }
+
+        public record class AssessmentResultResponseModel
+        {
+            public double Score { get; set; }
+            public ICollection<QuestionResponseView> Questions { get; set; }
+        }
+
+        public record class AssessmentResponseView
+        {
+            public ICollection<QuestionResponseView> Question { get; set; }
+            public QuestionType QuestionType { get; set; }
+            public DateTime Date { get; set; }
+        }
+    }
+}
