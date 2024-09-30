42106acb9bbeb95d51676379adae48e65d4ad548
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-30 23:41:22 +0100

    wrapper  model

---
+++ GateWay/Models/Wrapper/Wrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.ClientAI.Models.Wrapper
+{
+    public class Wrapper
+    {
+        public interface IResult
+        {
+            List<string> Messages { get; set; }
+
+            bool Succeeded { get; set; }
+        }
+
+        public interface IResult<out T> : IResult
+        {
+            T Data { get; }
+        }
+    }
+}
