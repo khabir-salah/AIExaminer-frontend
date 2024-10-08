482686e41ab095965b7dc059b89f2ad5df95d4f6
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-08 16:59:02 +0100

    modified

--- GateWay/Models/Authentication/CurrentUser.cs
+++ GateWay/Models/Authentication/CurrentUser.cs
@@ -2,10 +2,18 @@
 
 namespace Gateway.Client.Models.Authentication
 {
-    //public class CurrentUser
-    //{
-    //    public bool IsAuthenticated { get; set; }
-    //    public string UserName { get; set; }
-    //    public Dictionary<string, string> Claims { get; set; }
-    //}
+    public class CurrentUser
+    {
+        public bool IsAuthenticated { get; set; }
+        public string UserName { get; set; }
+        public Dictionary<string, string> Claims { get; set; }
+    }
+
+    public class TokenResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public string UserName { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
 }
