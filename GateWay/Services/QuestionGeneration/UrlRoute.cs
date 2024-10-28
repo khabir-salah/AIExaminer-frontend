41b5022e2960bb8ed4127365716c1c2341a8a81b
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-28 22:00:32 +0100

    Toast service added

--- GateWay/Services/QuestionGeneration/UrlRoute.cs
+++ GateWay/Services/QuestionGeneration/UrlRoute.cs
@@ -8,10 +8,14 @@ namespace Gateway.ClientAI.Services.QuestionGeneration
 {
     public class UrlRoute
     {
-        public static string RetakeAssessment = "api/assessment/RetakeAssessment/";
+        public static string RetakeAssessment = "api/assessment/RetakeAssesment/";
+        public static string TakeAssessment = "api/assessment/TakeAssessment/";
         public static string Generate = "api/assessment/GenerateTextQuestions";
         public static string Result = "api/assessment/AssessmentResult";
+        public static string AssessmentResult = "api/assessment/Result";
         public static string TextDetails = "api/assessment/GetTextAssessment";
+
+        
 
     }
 }
