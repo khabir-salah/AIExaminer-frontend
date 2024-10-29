47a3d10412e0e76cd7442c0103ea83b43e5894a2
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-29 23:14:08 +0100

    paystack response model

---
+++ GateWay/Models/Authentication/PaystackResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AI.Client.GateWay.Models.Authentication
+{
+    public class PaystackResponse
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public PaystackData Data { get; set; }
+    }
+
+    public class PaystackData
+    {
+        [JsonPropertyName("authorization_url")]
+        public string AuthorizationUrl { get; set; }
+
+        [JsonPropertyName("access_code")]
+        public string AccessCode { get; set; }
+
+        [JsonPropertyName("reference")]
+        public string Reference { get; set; }
+    }
+}
