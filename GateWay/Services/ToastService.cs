acf7efe1ce1590617370a739a8ed29241b169ba9
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-11-03 22:23:48 +0100

    Toast feature added

---
+++ GateWay/Services/ToastService.cs
@@ -0,0 +1,28 @@
+﻿using BlazorBootstrap;
+
+namespace AI.Client.GateWay.Services
+{
+    public class ToastService
+    {
+        public event Action<string, ToastType>? OnShow;
+        public event Action? OnHide;
+
+        public void ShowToast(string message, ToastType type)
+        {
+            OnShow?.Invoke(message, type);
+        }
+
+        public void HideToast()
+        {
+            OnHide?.Invoke();
+        }
+    }
+
+    public enum ToastType
+    {
+        Success,
+        Error,
+        Warning,
+        Info
+    }
+}
